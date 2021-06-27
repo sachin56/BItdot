2a2057da3a710d654e54ad6cfcfadc4f409e3fcf
sachin <heshanf45@gmail.com>
2021-06-27 23:03:29 +0530

    Show Leave Employee in Hr page

--- app/Http/Livewire/Admin/HrAdminComponent.php
+++ app/Http/Livewire/Admin/HrAdminComponent.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\EmployeeLeave;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class HrAdminComponent extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.admin.hr-admin-component')->layout('layouts.admin');
+        $hr=EmployeeLeave::paginate(10);
+        return view('livewire.admin.hr-admin-component',['hr'=>$hr])->layout('layouts.admin');
     }
 }
--- resources/views/livewire/admin/hr-admin-component.blade.php
+++ resources/views/livewire/admin/hr-admin-component.blade.php
@@ -1,3 +1,55 @@
 <div>
-    <h1>HR DashBoard</h1>
+    <style>
+        nav svg{
+            height: 20px;
+        }
+        nav .hidden{
+            display: block !important;
+        }
+    </style>
+    <div class="container" style="padding: 30px 0;">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-md-6">
+                                Attendent
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th>Employee Id</th>
+                                <th>Description</th>
+                                <th>Status</th>
+
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($hr as $task)
+                                <tr>
+                                    <td>{{$task->employee_id}}</td>
+                                    <td>{{$task->description}}</td>
+                                    <td>{{$task->status}}</td>
+                                    <td></td>
+                                    {{--                                        <a href="{{route('admin.editproducts',['product_slug'=>$product->slug])}}"><i class="fa fa-edit fa-2x text-info"></i> </a>--}}
+                                    {{--                                        <a href="#" style="margin-left: 10px;" wire:click.prevent="deleteProduct({{$product->id}})"><i class="fa fa-times fa-2x text-danger"></i> </a>--}}
+                                    {{--                                    </td>--}}
+
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                        {{$hr->links()}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+
+
